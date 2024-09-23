edc08415e92a27a9935a70edd4a4677be0196a47
kechcole <kechircollins@gmail.com>
2024-09-23 14:06:37 +0300

    Topic 2 : Spectral Indicies

---
+++ 2.Spectral_Indicies_Landsatimages.R
@@ -0,0 +1,17 @@
+'''
+              SPECTRAL INDICIES. 
+Spectral indicies compares the spectral reflectance from two or more wavelengths showing relative 
+abundance of a feature of interest. This analysis is commonly used to study vegetation, burnt areas, 
+built-up areas, water and geological features.  
+
+
+'''
+
+# Load libraries 
+
+library(raster)      # raster data
+library(rasterVis)  # raster visualisation  
+library(sp)         # Spatial data processing           
+# library(rgdal)      # Spatial data processing
+library(RStoolbox)  # Image analysis
+library(ggplot2)    # ploting
