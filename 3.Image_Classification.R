d56c86e697d85e92cdb0868463fa1f720d9cca8b
kechcole <kechircollins@gmail.com>
2024-09-26 15:56:31 +0300

    #

--- 3.Image_Classification.R
+++ 3.Image_Classification.R
@@ -3,7 +3,9 @@
 This is the process of assingning new land cover classes to an image resulting to thematic maps that 
 show land use maps. This activity on the reflactance value of different features, i.e a built up area
 has different value a s compared to vegetative areas. By assigning classes, the image acquires new 
-labels / themes. 
+labels / themes. Classification can be based on spectral values, spatial(textural and proximity) or 
+temporal imformation. The two kost commonly used methods are supervised and unsupervised, and have been in use 
+for a long time. Imprtant to note, they are pixel based and dependent on spectaral information, DN values. 
 
           Procedure of classification. 
 1. Image Preprocessing. Includes corrections i.e radiometric(missmatch between energy being emmited and one received), 
@@ -30,13 +32,6 @@ Remote sensing has a wide array of classifications groups based on characteristi
 
 I will delve into two major ones : supervided and supervised since they are commonly used and have been explored widely. 
 
-         Supervised Image Classification. 
-Using a set of pre-labelled sample data, an analyst trains a classifier that will then use knowledge learnt to classify an image.
-Once an algorithm learns, it is exposed to unknown pixels and assign them to different classes. Sample data data can be fetched 
-from field visits, expert knowledge, or ancilliary data such as maps. Example of such algorithms include random forest, support vector 
-machine, and decision trees. One huge advantage is that this algorithm meets user objectives or needs resulting to less effort and 
-time/cost saving due to higher accuracies. The downside is that in real world scenario, data is homegenious this it does not capture 
-natures variabilities and complexities. Users can also spend a lot of time when collecting sampling data.  
 
         Unsupervised classification. 
 This algorithm does not have any prio knowledge of the data and uses pixel properties such as simmilaries or differences in radiance value
@@ -97,6 +92,76 @@ print(paste0(dataFolder, 'Unsup_Kmeans.tif'))
 
 
 
-# ---------------------------------------------------------------------------------
-#                    Section 2 : SUPERVISED CLASSIFICATION.
-#----------------------------------------------------------------------------------
+''' ---------------------------------------------------------------------------------
+                    Section 2 : SUPERVISED CLASSIFICATION.
+Using a set of pre-labelled sample data, an analyst trains a classifier that will then use knowledge learnt to classify an image.
+Once an algorithm learns, it is exposed to unknown pixels and assign them to different classes. Sample data data can be fetched 
+from field visits, expert knowledge, or ancilliary data such as maps. Input class information is based on prior knowledge of the user, this 
+will guide how pixels will be grouped, these bounds are based on brighthness/spectral reflactance characteristics. 
+Example of such algorithms include random forest, support vector 
+machine, and decision trees. One huge advantage is that this algorithm meets user objectives or needs resulting to less effort and 
+time/cost saving due to higher accuracies. The downside is that in real world scenario, data is homegenious this it does not capture 
+natures variabilities and complexities. Users can also spend a lot of time when collecting sampling data.  
+
+                  Steps In Supervised Classification. 
+      a) Select Training Samples. 
+Define the number of feature class of interest to your study and generate training samples for each. Can be through a digitsation process 
+where a polygons or points are drawn, ensure it covers majority of the image and classes are distributed. 
+ 
+      b) Generate Signature File.
+Collected data values for different classes need to be stored for future reference in signature file. A spectral signature is the DN values, 
+it defines a wavelength pattern for GCP and represent a certain class.  
+
+      c) Classify. 
+Finally, as an analyst will classify the image using methods such as Random forest, Maximum Likelihood, SVM, Iso-cluster, PCA, 
+Maximum distance etc. 
+
+
+                  2.1 Random Forest. 
+This is an ensemble algorithm that involves many decison tree, each with a prediction that is then aggregated and the class with most votes 
+takes the day. Each tree  
+
+
+'''
+
+packages <- c("randomForest", "plyr", "RStoolbox", "RColorBrewer", "doParallel")
+
+ipak <- function(pkg){
+   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+   if (length(new.pkg)) 
+       install.packages(new.pkg, dependencies = TRUE)
+   sapply(pkg, require, character.only = TRUE)
+ }
+
+
+ipak((packages))
+
+library(caret)        # machine laerning
+library(randomForest) # Random Forest
+# library(rgdal)        # spatial data processing
+library(raster)       # raster processing
+library(plyr)         # data manipulation 
+library(dplyr)        # data manipulation 
+library(RStoolbox)    # ploting spatial data 
+library(RColorBrewer) # color
+library(ggplot2)      # ploting
+library(sp)           # spatial data
+library(doParallel)   # Parallel processing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
