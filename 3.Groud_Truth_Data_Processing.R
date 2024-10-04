9bd6b991ab8d2e17080dbbae202b888eb85243c2
kechcole <kechircollins@gmail.com>
2024-10-04 16:44:39 +0300

    #

--- 3.Groud_Truth_Data_Processing.R
+++ 3.Groud_Truth_Data_Processing.R
@@ -20,37 +20,41 @@ dataFolder <- "c:/Users/admin/Downloads/Nairobi Landsat data/"
 # Load raster data 
 landsat_24 <- stack(paste0(dataFolder, 'NAIROBI_L8_2023.tif'))
 crs(landsat_24)
-plot(landsat_24)
+
+# Subset bands and plot 
+selected_bands <- stack(landsat_24[[2:7]])
+plot(selected_bands)
 
 # Reproject raster 
 # You can use an EPSG code (32337 for UTM Zone 37S, WGS 84 datum) or a proj4string
-target_crs <- CRS("+proj=utm +zone=37 +datum=WGS84 +units=m +no_defs")
+target_crs <- CRS("+init=EPSG:32337")
 
 # Step 3: Reproject the raster
-landsat_rpUTM <- projectRaster(landsat_24, crs = target_crs)
+landsat_rpUTM <- projectRaster(selected_bands, crs = target_crs)
 plot(landsat_rpUTM)
 
 
+# Step 4. Load vector data and reproject to same CRS as raster 
 # Load geopackage using sf package , layer is train_2023
 polygon_layers <- st_layers(paste0(dataFolder, 'Nairobidata.gpkg'))
 polygon_layers
 
 # Load a single layer 
 polygon1 <- st_read(paste0(dataFolder, 'Nairobidata.gpkg'), layer = 'dissolved')
-
-# CRS 
 crs(polygon1)
 
 # Reproject to Pseudo Mercator, projected coordinate system that uses meters 
-poly_rep <- st_transform(polygon1, crs=3857) 
-poly_rep
+poly_rpUTM <- st_transform(polygon1, crs=32337) 
+crs(poly_rpUTM)
 
-# Choose attribute 
-attribute <- poly_rep$Class_Name 
+
+#Step 4. 
+# Choose attribute to be Plotted 
+attribute <- poly_rpUTM$Class_Name 
 
 # Plot
-plot(poly_rep["Class_Name"], col = terrain.colors(length(unique(attribute))), 
-     main = "Plot Colored by Land Type.")
+plot(poly_rpUTM["Class_Name"], col = terrain.colors(length(unique(attribute))), 
+     main = "Digitized Areas Colored by Land Type.")
 
 # Add a legend
 legend("bottomright", legend = unique(attribute), 
@@ -63,23 +67,24 @@ legend("bottomright", legend = unique(attribute),
 # Convert Polygon to raster
 
 # Get extent from raster 
-extent <- extent(landsat_24)
+extent <- extent(selected_bands)
 extent
 
 # Get projection of polygon to be used in rasterisation 
-crs = st_crs(poly_rep)$proj4string
+crs = st_crs(poly_rpUTM)$proj4string
 crs
 
-# Create a blank raster (define resolution and extent)
+# Create a blank raster (define resolution of 2.5m  and extent)
 # Define the raster resolution (in the units of the coordinate system)
-rast_template <- raster(extent, resolution=2.5, crs = crs)
+rast_template <- raster(extent, resolution=2.5, 
+        crs = "+proj=utm +zone=37 +south +ellps=WGS72 +datum=WGS84 +units=m +no_defs")
 
 # Assign the extent of reprojected polygon to raster template 
-extent(rast_template) <- extent(poly_rep)
+extent(rast_template) <- extent(poly_rpUTM)
 
 # Rasterize the polygon based on an attribute
 # Choose the attribute for rasterization MUST be a numeric function
-rp <- rasterize(poly_rep, rast_template, 'Class_ID', fun = "first")
+rp <- rasterize(poly_rpUTM, rast_template, 'Class_ID')
 
 # Plot 
 plot(rp, main="Ground truth data")
